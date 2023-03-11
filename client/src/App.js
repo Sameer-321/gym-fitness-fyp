dd8d008c15459dda9f64b7c0feb48aa20fe0d0f9
Sameer-321 <snrsameer1@gmail.com>
2023-03-11 18:41:13 +0545

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,11 @@ import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 import { Routes, Route } from "react-router-dom";
 import LoginRegister from "./pages/LoginRegister";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // import { isLoggedIn } from "./features/auth/authSlice";
-import { err, token,getToken } from "./features/auth/authSlice";
+import { err, token, getToken } from "./features/auth/authSlice";
 
 function App() {
-  //const jwt = useSelector(token);
-//   const dispatch = useDispatch()
-// const jwt = dispatch(getToken())
-//   const [token,setToken]=useState(null);
-//   // console.log(select)
-
-//   useEffect(() => {
-//     setToken(jwt)
-//     console.log(token)
-//   },[jwt]);
-
-
   return (
     <div>
       <Routes>
@@ -30,6 +18,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/login" element={<LoginRegister />} />
           <Route path="*" element={<Notfound />} />
+          
           {/* //protected route
           <Route element={<RequireAuth/>} >
             <Route path="asdf" element/>
