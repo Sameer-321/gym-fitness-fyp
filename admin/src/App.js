86ead21376d240d818a3b271cd55b33181aa765a
Sameer-321 <snrsameer1@gmail.com>
2023-03-28 19:38:03 +0545

    user auth and sign in UI

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,13 @@
 import { AdminFrame } from "./components/AdminFrame";
-
+import {Login} from "./components/Login";
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <AdminFrame/>
     </div>
-  );
+  )
 }
 
 export default App;
