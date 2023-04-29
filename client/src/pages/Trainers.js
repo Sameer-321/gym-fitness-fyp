8fc206ce7c5e516d346708119ba8262e50ee07a4
Sameer-321 <snrsameer1@gmail.com>
2023-04-29 13:35:20 +0545

    UI updates

--- client/src/pages/Trainers.js
+++ client/src/pages/Trainers.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 export default function TrainerRender() {
   const URL = "http://localhost:5000/api/v1";
   const [trainers, setTrainers] = useState([]);
@@ -76,13 +77,41 @@ const TrainerPageCard = ({ detail }) => {
           </div>
         </td>
 
-        <td className="px-1 py-1 text-xs border">
+        <td className="px-1 py-1 text-xs border relative">
+          <div className=" bg-purple-600 rounded-full text-center w-2/5 m-auto my-2 h-auto p-1">
+            <span className="font-semibold text-stone-200 tracking-wide">
+              Years of Experience:
+            </span>
+            <span className="font-bold text-slate-900 text-[15px] pt-[12px]">
+              {detail.yearsOfExperience}
+            </span>
+          </div>
+          <div className="flex justify-start items-center  flex-wrap my-2">
+            <div className="w-1/5 font-bold text-[16px] text-purple-600">
+              Fields :
+            </div>
+            <div className="w-4/5">
+              {detail.trainerType.map((i) => {
+                return (
+                  <p className=" bg-blue-200 text-center font-semibold tracking-wide text-white-600 rounded-full inline-block w-2/5 p-1 m-1 ">
+                    {i}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
           <p className="px-2   text-blue-950 hover:bg-gray-300 text-justify">
             {detail.description}
           </p>
-          <button className="rounded-lg  my-1 mb-0 px-4 py-2 font-bold text-gray-50 bg-purple-600 hover:bg-purple-900">
+          <button className="rounded-lg mx-auto my-1 mb-2 px-4 py-2 font-bold text-gray-50 bg-purple-600 hover:bg-purple-900">
             Hire(Rs 5000)
           </button>
+          <div className="absolute bottom-2 right-5 text-purple-400 hover:text-purple-700">
+            <div className="inline-block">View More </div>
+            <div className="w-5 h-5 inline-block pt-2">
+              <ArrowRightIcon />
+            </div>
+          </div>
         </td>
       </tr>
     </>
